697cd29ab37573f954992b907fc7939d3fc42983
donhyuck <leedonhyuck123@gmail.com>
2022-04-23 15:44:45 +0900

    findById 1번 질문 찾기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -43,6 +44,19 @@ class SbbApplicationTests {
         // 1번 질문 테스트
         Question q = all.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+    }
+
+    @Test
+    void testFindById() {
+
+        // 1번 질문
+        Optional<Question> oq = questionRepository.findById(1);
+
+        if (oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
 
     }
 }
