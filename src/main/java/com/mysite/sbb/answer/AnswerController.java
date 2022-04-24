43d387483f38cebfddf75db4316e150ea29f5a0f
donhyuck <leedonhyuck123@gmail.com>
2022-04-24 16:02:45 +0900

    답변 등록 추가, 답변 작성하고 질문페이지에서 표시하기

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,32 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/answer")
+public class AnswerController {
+
+    @Autowired
+    QuestionService questionService;
+    @Autowired
+    AnswerService answerService;
+
+    @PostMapping(value = "/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+
+        Question question = questionService.getQuestion(id);
+        answerService.create(question, content);
+
+        model.addAttribute("question", question);
+
+        return "redirect:/question/detail/%d".formatted(id);
+    }
+}
