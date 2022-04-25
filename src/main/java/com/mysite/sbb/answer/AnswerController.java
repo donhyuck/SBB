0c7dcfa6c1fda337a6839136dbbce50368ac4088
donhyuck <leedonhyuck123@gmail.com>
2022-04-25 16:51:33 +0900

    답변 등록 기능 개선, 내용 입력확인

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -5,10 +5,12 @@ import com.mysite.sbb.question.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/answer")
@@ -20,13 +22,16 @@ public class AnswerController {
     AnswerService answerService;
 
     @PostMapping(value = "/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
 
         Question question = questionService.getQuestion(id);
-        answerService.create(question, content);
 
-        model.addAttribute("question", question);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
 
+        answerService.create(question, answerForm.getContent());
         return "redirect:/question/detail/%d".formatted(id);
     }
 }
